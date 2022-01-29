d18410c0d906ce84a1175910b94c644a9b5d50ef
Maan2003 <manmeetmann2003@gmail.com>
2022-01-29 21:41:12 +0530

    add dummy data in db.py

--- src/db.py
+++ src/db.py
@@ -12,7 +12,18 @@ def list_files(query: str, tags: list[str]) -> list[File]:
     List files in database that match query and tags.
     """
     # TODO: query from db
-    return []
+    return [
+        File(
+            name="file1",
+            link="https://example.com/file1",
+            tags=["tag1", "tag2"]
+        ),
+        File(
+            name="file2",
+            link="https://example.com/file2",
+            tags=["tag1", "tag2"]
+        ),
+    ]
 
 
 def add_files(files: list[File]):
