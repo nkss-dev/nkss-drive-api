6285752b52c821a4a93b72fae467346626de1fcf
Shubham Patel <shubhamexperiment1@gmail.com>
2022-02-02 00:53:47 +0530

    flask added

--- src/app.py
+++ src/app.py
@@ -1,4 +1,13 @@
 # TODO flask
+from flask import Flask
 import db
 
+app = Flask(__name__)
+
 # add a route that calls db.list_files()
+@app.route("/files")
+def files():
+   return db.list_files()
+
+if __name__ == "__main__":
+   app.run(debug=True, port=8000)
