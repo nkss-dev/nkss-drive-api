ec0557a3a4674a3568c000623ce148fe3e693970
TechsavvYash <mittal.yash12@hotmail.com>
2022-02-02 21:33:31 +0530

    added code to fetch data from the drive
    Signed-off-by: TechsavvYash <mittal.yash12@hotmail.com>

--- src/db.py
+++ src/db.py
@@ -1,23 +1,66 @@
+
+import sqlite3
 from dataclasses import dataclass
+
+
+# queries to create the db tables
+
+
 
 @dataclass
 class File:
     name: str
     link: str
-    tags: list[str]
+    tags: "list[str]"
 
 
-def list_files(query: str, tags: list[str]) -> list[File]:
+
+def create_tables():
+    db = sqlite3.connect('files.db')
+    cursor = db.cursor()
+    cursor.execute('''
+create table if not exists files(
+  fid         int auto_increment primary key,
+  name        text not null,
+  drive_url   text not null
+);''')
+    cursor.execute('''
+create table if not exists tags(
+  tid         int primary key,
+  name        text not null
+);
+''')	
+    cursor.execute('''
+	create table if not exists file_tags(
+  	tid         int,
+  fid         int,
+  primary key (tid, fid)
+  );
+''')
+    db.commit()
+    db.close()
+
+def list_files(query: str, tags: "list[str]") -> "list[File]":
     """
     List files in database that match query and tags.
     """
     # TODO: query from db
+    query = f""
+
     return []
 
 
-def add_files(files: list[File]):
+def add_files(files: "list[File]"):
     """
     Add files to database.
     """
     # TODO: add to db
+    db = sqlite3.connect('files.db')
+    cursor = db.cursor()
+    for file in files:
+        query = f"INSERT INTO files(name, drive_url) VALUES ('{file.name}', '{file.link}');"
+        cursor.execute(query)
+        db.commit()
+        print("added", file.name)
     pass
+
--- src/drive.py
+++ src/drive.py
@@ -1,10 +1,79 @@
-import db
+# import db
+from Google import connect_to_drive
+import pandas as pd
+import json
+
+
+def get_folder_contents(service, id):
+
+    query = f"parents = '{id}'"
+    if not service:
+        print("Connection failed")
+        return []
+
+    response = service.files().list(q=query).execute()
+    files = response.get('files', [])
+    nextPageToken = response.get('nextPageToken')
+    
+    while nextPageToken:
+        response = service.files().list(q=query, pageToken=nextPageToken).execute()
+        files.extend(response.get('files'))
+        nextPageToken = response.get('nextPageToken')
+
+    return files
+
+
+def list_drive(service, folder_id, result, parentName='', grandParentName=''):
+    files = get_folder_contents(service, folder_id)
+    
+    for item in files:
+        if item['mimeType'] != 'application/vnd.google-apps.folder':
+            try:
+                id = item['id']
+                link = (service.files().get(fileId=id, fields='webViewLink').execute())[
+                    'webViewLink']
+                item['tags'] = [parentName, grandParentName]
+                item['drive_url'] = link
+                result.append(item)
+            except Exception as error:
+                print("Error: ", error)
+
+    for item in files:
+        # parentName = 'General'
+        if item['mimeType'] == 'application/vnd.google-apps.folder':
+            if parentName != 'General':
+                grandParentName = parentName
+            parentName = item['name']
+            folder_id = item['id']
+            # contents = get_folder_contents(service, folder_id)
+            try:
+                list_drive(service, folder_id, result,
+                           parentName, grandParentName)
+            except Exception as error:
+                print("Error: ", error)
+
 
 def main():
     # TODO: query the drive and add files to the database
     # call db.add_files() to add files to the database
+
+    folder_id = '1U2taK5kEhOiUJi70ZkU2aBWY83uVuMmD'
+    CLIENT_SECRET_FILE = '../credentials.json'
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+    service = connect_to_drive(CLIENT_SECRET_FILE, SCOPES)
+    if not service:
+        print("Connection failed")
+        exit()
+
+    print("Drive Connected!")
+    result = []
+    list_drive(service, folder_id, result, 'General', '')
+    files = pd.DataFrame(result)
+    print(files)
+    files.to_excel("links.xlsx")
+
     pass
+
 
 if __name__ == '__main__':
     main()
-
