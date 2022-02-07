0d3a0ec3da904519288c392b51317587ddc2ad90
ShubhamBhut <shubhamprecious1@gmail.com>
2022-02-08 00:19:14 +0530

    Schema: fix auto increment

--- src/schema.sql
+++ src/schema.sql
@@ -1,16 +1,16 @@
 create table if not exists files(
-  fid         int auto_increment primary key,
+  fid         INTEGER primary key AUTOINCREMENT,
   name        text not null,
   drive_url   text not null
 );
 
 create table if not exists tags(
-  tid         int primary key,
+  tid         INTEGER primary key AUTOINCREMENT,
   name        text not null
 );
 
 create table if not exists file_tags(
-  tid         int,
-  fid         int,
+  tid         INTEGER,
+  fid         INTEGER,
   primary key (tid, fid)
 );
