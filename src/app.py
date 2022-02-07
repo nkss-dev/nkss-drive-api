09231b1b0c5e25a6d6bd2811a03c281fcff54933
Maan2003 <manmeetmann2003@gmail.com>
2022-02-08 02:17:46 +0530

    db: implement list_files

--- src/app.py
+++ src/app.py
@@ -2,10 +2,11 @@ from flask import Flask, jsonify, request
 import db
 
 app = Flask(__name__)
-db.connect("./data/db.sqlite3")
 
 @app.route('/files', methods=['GET'])
 def files():
+   # FIXME: don't do this every time
+   db.connect("./data/db.sqlite3")
    tags = request.args.get("tags")
    if tags is None: 
       tags = []
