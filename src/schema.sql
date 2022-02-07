b576661ada542ee71bcc542e7f7e34799e244a71
ShubhamBhut <shubhamexperiment1@gmail.com>
2022-02-07 23:15:20 +0530

    fix autoincrement syntex

--- src/schema.sql
+++ src/schema.sql
@@ -1,11 +1,11 @@
 create table if not exists files(
-  fid         int auto_increment primary key,
+  fid         INTEGER primary key AUTOINCREMENT,
   name        text not null,
   drive_url   text not null
 );
 
 create table if not exists tags(
-  tid         int primary key,
+  tid         INTEGER primary key AUTOINCREMENT,
   name        text not null
 );
 
